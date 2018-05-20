c5ded03b931323fd19a798682ad716073c71cb4d
Siobhan Williams <vonnie2k5@gmail.com>
2018-05-19 23:05:59 -0400

    Implement ARIA checkbox

--- lesson5-semantics-aria/03-first-steps/checkboxes.js
+++ lesson5-semantics-aria/03-first-steps/checkboxes.js
@@ -1,13 +1,13 @@
-(function() {
+(function () {
   'use strict';
 
   /** Define values for keycodes */
-  var VK_ENTER      = 13;
-  var VK_SPACE      = 32;
-  var VK_LEFT       = 37;
-  var VK_UP         = 38;
-  var VK_RIGHT      = 39;
-  var VK_DOWN       = 40;
+  var VK_ENTER = 13;
+  var VK_SPACE = 32;
+  var VK_LEFT = 37;
+  var VK_UP = 38;
+  var VK_RIGHT = 39;
+  var VK_DOWN = 40;
 
   /** Helper function to convert NodeLists to Arrays */
   function slice(nodes) {
@@ -21,28 +21,34 @@
     this.el.addEventListener('click', this.toggle.bind(this));
 
     // Any other set-up we want to do here?
+    if (this.el.hasAttribute("checked")) {
+      this.el.setAttribute("aria-checked", "true");
+    } else {
+      this.el.setAttribute("aria-checked", "false");
+    }
   }
 
-  Checkbox.prototype.handleKeyDown = function(e) {
-    switch(e.keyCode) {
+
+  Checkbox.prototype.handleKeyDown = function (e) {
+    switch (e.keyCode) {
       case VK_ENTER:
-      case VK_SPACE: {
-        this.toggle();
-        break;
-      }
+      case VK_SPACE:
+        {
+          this.toggle();
+          break;
+        }
     }
   };
 
-  Checkbox.prototype.toggle = function() {
+  Checkbox.prototype.toggle = function () {
     if (this.el.hasAttribute('checked')) {
       this.el.removeAttribute('checked');
+      this.el.setAttribute("aria-checked", "false");
 
-      // Hmm.
 
     } else {
       this.el.setAttribute('checked', '');
-
-      // Hmmmmm.
+      this.el.setAttribute("aria-checked", "true");
 
     }
   };
